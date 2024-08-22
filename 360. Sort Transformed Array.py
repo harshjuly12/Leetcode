332da9712ef9be91741489e8f44af34ac798ce4f
‎ ‎ ‎ ‎ ‎ ‎ ‎ 👨🏻‍💻Harsh Singh🐢 <harshjuly12@gmail.com>
2024-08-22 15:22:15 +0530

    Add files via upload

---
+++ 360. Sort Transformed Array.py
@@ -0,0 +1,39 @@
+class Solution:
+  def sortTransformedArray(
+      self,
+      nums: list[int],
+      a: int,
+      b: int,
+      c: int,
+  ) -> list[int]:
+    n = len(nums)
+    upward = a > 0
+    ans = [0] * n
+
+    def f(x: int, a: int, b: int, c: int) -> int:
+      return (a * x + b) * x + c
+
+    quad = [f(num, a, b, c) for num in nums]
+
+    i = n - 1 if upward else 0
+    l = 0
+    r = n - 1
+    while l <= r:
+      if upward:  
+        if quad[l] > quad[r]:
+          ans[i] = quad[l]
+          l += 1
+        else:
+          ans[i] = quad[r]
+          r -= 1
+        i -= 1
+      else:  
+        if quad[l] < quad[r]:
+          ans[i] = quad[l]
+          l += 1
+        else:
+          ans[i] = quad[r]
+          r -= 1
+        i += 1
+
+    return ans
