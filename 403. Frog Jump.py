f9d557225259808f9bb782fc3f697bff19a837ab
‎ ‎ ‎ ‎ ‎ ‎ ‎ 👨🏻‍💻Harsh Singh🐢 <harshjuly12@gmail.com>
2024-08-27 17:52:19 +0530

    Add files via upload

---
+++ 403. Frog Jump.py
@@ -0,0 +1,16 @@
+class Solution:
+  def canCross(self, stones: list[int]) -> bool:
+    n = len(stones)
+    dp = [[False] * (n + 1) for _ in range(n)]
+    dp[0][0] = True
+
+    for i in range(1, n):
+      for j in range(i):
+        k = stones[i] - stones[j]
+        if k > n:
+          continue
+        for x in (k - 1, k, k + 1):
+          if 0 <= x <= n:
+            dp[i][k] |= dp[j][x]
+
+    return any(dp[-1])
