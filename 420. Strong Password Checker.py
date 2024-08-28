ce8c5fefe9dedf31640aacf68706e13f0bf22474
‎ ‎ ‎ ‎ ‎ ‎ ‎ 👨🏻‍💻Harsh Singh🐢 <harshjuly12@gmail.com>
2024-08-28 11:30:40 +0530

    Add files via upload

---
+++ 420. Strong Password Checker.py
@@ -0,0 +1,44 @@
+class Solution:
+  def strongPasswordChecker(self, password: str) -> int:
+    n = len(password)
+    missing = self._getMissing(password)
+    # the number of replacements to deal with 3 repeating characters
+    replaces = 0
+    # the number of sequences that can be substituted with 1 deletions,
+    # (3k)-seqs
+    oneSeq = 0
+    # the number of sequences that can be substituted with 2 deletions,
+    # (3k + 1)-seqs
+    twoSeq = 0
+
+    i = 2
+    while i < n:
+      if password[i] == password[i - 1] and password[i - 1] == password[i - 2]:
+        length = 2 
+        while i < n and password[i] == password[i - 1]:
+          length += 1
+          i += 1
+        replaces += length // 3  
+        if length % 3 == 0:
+          oneSeq += 1
+        if length % 3 == 1:
+          twoSeq += 1
+      else:
+        i += 1
+
+    if n < 6:
+      return max(6 - n, missing)
+    if n <= 20:
+      return max(replaces, missing)
+
+    deletes = n - 20
+    replaces -= min(oneSeq, deletes)
+    replaces -= min(max(deletes - oneSeq, 0), twoSeq * 2) // 2
+    replaces -= max(deletes - oneSeq - twoSeq * 2, 0) // 3
+    return deletes + max(replaces, missing)
+
+  def _getMissing(self, password: str) -> int:
+    return (3
+            - any(c.isupper() for c in password)
+            - any(c.islower() for c in password)
+            - any(c.isdigit() for c in password))
